60a48b59815d50c8e15fa5c9fea3a96bf66b958f
T O <tariqaliomar@hotmail.com>
2022-01-07 11:15:12 -0500

    API calls added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    internal class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Summary = "This phone is the biggest flagship phone",
+                    Description = "This is a dummy description",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
+    }
+}
