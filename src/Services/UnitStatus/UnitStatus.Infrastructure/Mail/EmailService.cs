a3e8611379ec3cc15f03707075b366add30c598b
T O <tariqaliomar@hotmail.com>
2022-02-21 11:29:57 -0500

    Added Email Service implementation in infrastructure

---
+++ src/Services/UnitStatus/UnitStatus.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using UnitStatus.Application.Contracts.Infrastructure;
+using UnitStatus.Application.Models;
+
+namespace UnitStatus.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if(response.StatusCode == HttpStatusCode.Accepted || response.StatusCode == HttpStatusCode.OK)
+                return true;
+            _logger.LogInformation("Email sending failed.");
+
+            return false;
+        }
+    }
+}
