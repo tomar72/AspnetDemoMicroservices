9a3f84f312692beefe4e283d3c9a0d9110f93892
T O <tariqaliomar@hotmail.com>
2022-02-21 11:53:54 -0500

    Registered services to application API

--- src/Services/UnitStatus/UnitStatus.API/Startup.cs
+++ src/Services/UnitStatus/UnitStatus.API/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using UnitStatus.Application;
+using UnitStatus.Infrastructure;
 
 namespace UnitStatus.API
 {
@@ -19,6 +21,8 @@ namespace UnitStatus.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
