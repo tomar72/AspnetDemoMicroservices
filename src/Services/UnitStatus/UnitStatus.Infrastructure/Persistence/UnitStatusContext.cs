225b31ffc84a7f6e98160dfb38d75a3822bdbe39
T O <tariqaliomar@hotmail.com>
2022-02-21 10:42:56 -0500

    Created DbContext for SQLServer Entity Framework

---
+++ src/Services/UnitStatus/UnitStatus.Infrastructure/Persistence/UnitStatusContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitStatus.Domain.Common;
+
+namespace UnitStatus.Infrastructure.Persistence
+{
+    public class UnitStatusContext : DbContext
+    {
+        public UnitStatusContext(DbContextOptions<UnitStatusContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Entities.UnitStatus> UnitStatuses { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swm";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swm";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/UnitStatus/UnitStatus.Infrastructure/Persistence/UnitStatusContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitStatus.Infrastructure.Persistence
+{
+    public class UnitStatusContextSeed
+    {
+        public static async Task SeedAsync(UnitStatusContext unittatusContext, ILogger<UnitStatusContextSeed> logger)
+        {
+            if (!unittatusContext.UnitStatuses.Any())
+            {
+                unittatusContext.UnitStatuses.AddRange(GetPreconfiguredStatuses());
+                await unittatusContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(UnitStatusContext).Name);
+            }
+        }
+
+        private static IEnumerable<Domain.Entities.UnitStatus> GetPreconfiguredStatuses()
+        {
+            return new List<Domain.Entities.UnitStatus>{
+                new Domain.Entities.UnitStatus() { Description = "My test unit status", IsOnHold = false } 
+            };
+        }
+    }
+}
