0bfa0b68a49b635c5a09a9c438041f9f4e537fc3
T O <tariqaliomar@hotmail.com>
2022-02-21 11:15:25 -0500

    Added Db Repository pattern for Unit Status

---
+++ src/Services/UnitStatus/UnitStatus.Infrastructure/Repositories/UnitStatusRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitStatus.Application.Contracts.Persistence;
+using UnitStatus.Infrastructure.Persistence;
+
+namespace UnitStatus.Infrastructure.Repositories
+{
+    public class UnitStatusRepository : RepositoryBase<Domain.Entities.UnitStatus>, IUnitStatusRepository
+    {
+        public UnitStatusRepository(UnitStatusContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Domain.Entities.UnitStatus>> GetUnitStatusesByUnitId(string unitId)
+        {
+            var orderList = await _dbContext.UnitStatuses
+                                        .Where(o => o.Description == unitId)
+                                        .ToListAsync();
+            return orderList;
+        }
+    }
+}
